470517ecdba5c48def47ed2f7cfe66e4433395fd
anthqiu <i@shiqiu.dev>
2026-02-25 22:20:41 -0500

    fix typo
    Signed-off-by: anthqiu <i@shiqiu.dev>

--- src-rust-crates/database/src/user_profile.rs
+++ src-rust-crates/database/src/user_profile.rs
@@ -2,5 +2,5 @@ use nutrack_model::user_profile::UserProfile;
 
 #[tauri::command]
 pub fn create_profile(user_profile: UserProfile) -> String {
-    format!("Created user profile {}, {}, weight {}, height {}", user_profile.name, user_profile.sex.to_string(), user_profile.height, user_profile.height)
+    format!("Created user profile {}, {}, weight {}, height {}", user_profile.name, user_profile.sex.to_string(), user_profile.weight, user_profile.height)
 }
